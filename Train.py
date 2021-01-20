efb9b6b2bf7edf3b02e3637912450105055c7cb8
Hung-Yu Wu <77454537+HungYu-Wu@users.noreply.github.com>
2021-01-20 16:05:04 +0800

    Update Train.py

--- Train.py
+++ Train.py
@@ -12,20 +12,7 @@ from torchstat import stat
 
 
 def structure_loss(pred, mask):
-    '''
-    N = pred.size(2)
-    smooth = 1
- 
-    input_flat = pred.view(N, -1)
-    target_flat = mask.view(N, -1)
- 
-    intersection = input_flat * target_flat
- 
-    loss = 2 * (intersection.sum(1) + smooth) / (input_flat.sum(1) + target_flat.sum(1) + smooth)
-    loss = 1 - loss.sum() / N
-
-    return loss
-    '''
+    
     weit = 1 + 5*torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
     wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
     wbce = (weit*wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
